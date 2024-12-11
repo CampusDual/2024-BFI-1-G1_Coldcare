109736c59bf9c141d1429fd0ddbad433a5a63eb4
Kurosaki <pablobasantadominguez@gmail.com>
2024-12-11 11:17:47 +0000

    Creación de la interfaz del servicio

---
+++ cd2024bfi1g1-api/src/main/java/com/campusdual/cd2024bfi1g1/api/core/service/IDeviceService.java
@@ -0,0 +1,14 @@
+package com.campusdual.cd2024bfi1g1.api.core.service;
+
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IDeviceService {
+
+    EntityResult deviceQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+    EntityResult deviceInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+
+}
---
+++ cd2024bfi1g1-api/src/main/java/com/campusdual/cd2024bfi1g1/api/core/service/IMeasurementService.java
@@ -0,0 +1,14 @@
+package com.campusdual.cd2024bfi1g1.api.core.service;
+
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IMeasurementService {
+
+    EntityResult measurementQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+    EntityResult measurementInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+
+}
