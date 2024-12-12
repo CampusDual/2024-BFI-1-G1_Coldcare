cbb4c51ec5d514e2b26ff4f1b1fc723678e3da9e
Alex Domínguez <doalex7@gmail.com>
2024-12-12 14:45:24 +0100

    Added filtered by mac inserts and fixed dao bug

--- cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/dao/MeasurementsDao.java
+++ cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/dao/MeasurementsDao.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Lazy
 @ConfigurationFile(configurationFile = "dao/MeasurementsDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 
-public class MeasurementsDao {
+public class MeasurementsDao extends OntimizeJdbcDaoSupport{
 
     public static final String ME_ID = "ME_ID";
     public static final String ME_TEMP = "ME_TEMP";
