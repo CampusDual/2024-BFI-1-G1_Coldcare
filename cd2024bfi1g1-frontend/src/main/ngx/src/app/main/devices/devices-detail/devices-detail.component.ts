2f0619474cfdf108944b5cf42e5659b72800cc98
Gabriel <gabriel.lamela.ch@gmail.com>
2025-01-08 10:55:11 +0100

    Create devices-detail component

---
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-detail/devices-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devices-detail',
+  templateUrl: './devices-detail.component.html',
+  styleUrls: ['./devices-detail.component.css']
+})
+export class DevicesDetailComponent {
+
+}
