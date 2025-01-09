f67d3f3705be5fb8730ea68092e37e12eb1c6fed
Gabriel <Gabrieldb050890@gmail.com>
2025-01-09 10:11:35 +0100

    corregido script

--- cd2024bfi1g1-model/src/main/resources/db/migration/V3__create_microcontroller_tables.sql
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V3__create_microcontroller_tables.sql
@@ -1,3 +1,3 @@
 ALTER TABLE public.devices ADD dev_name varchar;
 UPDATE public.devices SET dev_name = dev_mac WHERE dev_name is NULL;
-ALTER TABLE public.devices change COLUMN dev_name varchar NOT NULL;
+ALTER TABLE public.devices ALTER COLUMN dev_name SET NOT NULL;
