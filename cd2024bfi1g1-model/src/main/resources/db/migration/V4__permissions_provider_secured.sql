c698b969bca06a8d42fa518c3612e6081a815534
Kurosaki <pablobasantadominguez@gmail.com>
2025-01-09 13:14:34 +0000

    QueryRolePermission

---
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V4__permissions_provider_secured.sql
@@ -0,0 +1,10 @@
+INSERT INTO usr_server_permission VALUES('com.campusdual.cd2024bfi1g1.model.core.service.MeasurementsService/measurementsQuery')
+INSERT INTO usr_server_permission VALUES('com.campusdual.cd2024bfi1g1.model.core.service.MeasurementsService/measurementsInsert')
+INSERT INTO usr_server_permission VALUES('com.campusdual.cd2024bfi1g1.model.core.service.MeasurementsService/measurementsUpdate')
+INSERT INTO usr_server_permission VALUES('com.campusdual.cd2024bfi1g1.model.core.service.MeasurementsService/measurementsDelete')
+
+
+INSERT INTO usr_role_server_permission VALUES(17,1)
+INSERT INTO usr_role_server_permission VALUES(18,1)
+INSERT INTO usr_role_server_permission VALUES(19,1)
+INSERT INTO usr_role_server_permission VALUES(20,1)
