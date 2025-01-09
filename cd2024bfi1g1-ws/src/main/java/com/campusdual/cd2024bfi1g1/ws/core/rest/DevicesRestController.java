8caad95e646bff979e972fdcca55ec13669a9dd2
Miguel Dagraza <migueldagraza@gmail.com>
2025-01-09 08:31:38 +0100

    error en restcontroller

--- cd2024bfi1g1-ws/src/main/java/com/campusdual/cd2024bfi1g1/ws/core/rest/DevicesRestController.java
+++ cd2024bfi1g1-ws/src/main/java/com/campusdual/cd2024bfi1g1/ws/core/rest/DevicesRestController.java
@@ -1,6 +1,6 @@
 package com.campusdual.cd2024bfi1g1.ws.core.rest;
 
-import com.campusdual.cd2024bfi1g1.api.core.service.IMeasurementsService;
+import com.campusdual.cd2024bfi1g1.api.core.service.IDevicesService;
 import com.ontimize.jee.server.rest.ORestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
