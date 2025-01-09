5a86d736fbb08ea41c5bf6f2a0cc4fed878ec71a
Jorge González Fernández <j.gonzalezf@udc.es>
2025-01-09 11:21:44 +0100

    Container new form

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-routing.module.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { ContainersDetailsComponent } from './containers-details/containers-details.component';
 import { ContainersHomeComponent } from './containers-home/containers-home.component';
+import { ContainersNewComponent } from './containers-new/containers-new.component';
 
+export const CONTAINERS_MODULE_DECLARATIONS = [
+  ContainersDetailsComponent,
+  ContainersHomeComponent,
+  ContainersNewComponent
+]
 
-const routes: Routes = [{
-    path: '',
-    component: ContainersHomeComponent
-    }];
+const routes: Routes = [
+  { path: '', component: ContainersHomeComponent },
+  { path: 'new', component: ContainersNewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
