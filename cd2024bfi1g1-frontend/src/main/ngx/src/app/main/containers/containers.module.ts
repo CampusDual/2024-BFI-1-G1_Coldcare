850baa4ff82664d0cf31449df2c085549822bc53
Balexo <oscar.vallejo@outlook.com>
2025-01-09 08:43:01 +0100

    Containers-home correct files configuration

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers.module.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContainersRoutingModule } from './containers-routing.module';
-import { ContainersComponent } from './containers.component';
 import { ContainersHomeComponent } from './containers-home/containers-home.component';
 
 
 @NgModule({
   declarations: [
-    ContainersComponent,
-    ContainersHomeComponent
+    ContainersHomeComponent,
   ],
   imports: [
     CommonModule,
