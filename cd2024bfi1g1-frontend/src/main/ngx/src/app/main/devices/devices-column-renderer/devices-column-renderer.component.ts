826bc4a0b020d07bf670030d68652f90af8356fd
Alex Domínguez <doalex7@gmail.com>
2025-01-13 14:51:48 +0100

    Fixed bug in devices table

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-column-renderer/devices-column-renderer.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-column-renderer/devices-column-renderer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { OBaseTableCellRenderer } from 'ontimize-web-ngx';
 
 @Component({
@@ -7,22 +7,22 @@ import { OBaseTableCellRenderer } from 'ontimize-web-ngx';
   styleUrls: ['./devices-column-renderer.component.css']
 })
 export class DevicesColumnRendererComponent extends OBaseTableCellRenderer implements OnInit {
-  
+  @ViewChild('templateref', { read: TemplateRef, static: false }) public templateref: TemplateRef<any>;
   obj = {
-    "background-color": "green"
+    "color": "black"
   }
   isTimeElapsedGreaterThan15Min(cellVal):boolean{
     const time: number = 15*60*1000;
     const currentDate:  number = new Date ().getTime();
-    const dif: number = cellVal - currentDate;
+    const dif: number = currentDate - cellVal;
     return dif>time;
   }
   getCellData(value: any) {
     console.log("llamada a getcellData")
     if(this.isTimeElapsedGreaterThan15Min(value)){
-      this.obj["background-color"] = "red";
+      this.obj["color"] = "red";
     }else{
-      this.obj["background-color"] = "green";
+      this.obj["color"] = "black";
     }
 
     return value;
