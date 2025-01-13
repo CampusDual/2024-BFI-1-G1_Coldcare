5abf48bcf153afaa654ebf88957a49eaaf6d4ff9
Kurosaki <pablobasantadominguez@gmail.com>
2025-01-13 08:25:32 +0000

    CorrecionMenuLateralMeasurements

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -2,7 +2,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'medidas', name: 'MEASUREMENTS', icon: 'thermostat', route: '/main/medidas' },
   { id: 'contenedores', name: 'CONTAINERS', icon: 'local_shipping', route: '/main/containers' },
   {
     id: 'admin', name: 'ADMIN', tooltip: 'ADMIN', icon: 'admin_panel_settings',
