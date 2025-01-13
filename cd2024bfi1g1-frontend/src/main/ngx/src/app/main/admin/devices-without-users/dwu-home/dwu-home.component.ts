265cad639ef93c240fef1fc0638665b36a28dbc6
beeg <beegdev13@gmail.com>
2025-01-13 12:25:51 +0100

    corregida tabla en el front

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/devices-without-users/dwu-home/dwu-home.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/devices-without-users/dwu-home/dwu-home.component.ts
@@ -6,10 +6,6 @@ import { OTableComponent } from 'ontimize-web-ngx';
   styleUrls: ['./dwu-home.component.css']
 })
 
-export class DwuHomeComponent implements OnInit {
-  @ViewChild('myTable', { static: true }) table: OTableComponent;
-
-  ngOnInit(): void {
-    this.table.filterData({ usr_id: null });
-  }
+export class DwuHomeComponent  {
+  
 }
