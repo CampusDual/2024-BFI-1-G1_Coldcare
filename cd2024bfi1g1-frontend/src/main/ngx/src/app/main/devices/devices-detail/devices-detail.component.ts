a1201a033be502d2e3dcdb12ddd86ecaf67b7712
Gabriel <gabriel.lamela.ch@gmail.com>
2025-01-14 10:32:33 +0100

    Integrar gráfica en devices-detail, añadir pestañas, eliminar devices-graph

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-detail/devices-detail.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-detail/devices-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Expression, FilterExpressionUtils, OTableComponent } from 'ontimize-web-ngx';
+import { DataAdapterUtils, LineChartConfiguration, OChartComponent } from 'ontimize-web-ngx-charts';
 
 @Component({
   selector: 'app-devices-detail',
@@ -7,23 +9,54 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 })
 export class DevicesDetailComponent {
 
-  constructor() {
+  @ViewChild('measurementsTable', { static: false }) measurementsTable: OTableComponent;
+  @ViewChild('lineChartBasic', { static: false }) lineChart: OChartComponent;
 
+  dataArray: any = [];
+  chartData = [];
+
+  chartParameters: LineChartConfiguration;
+  
+  colorScheme = {
+    domain: ['#1464A5', '#eeeeee', '#c5c5c5']
+  };
+
+  constructor() {
+    this.chartParameters = new LineChartConfiguration();
+    this.chartParameters.isArea = [true];
+    this.chartParameters.interactive = false;
+    this.chartParameters.useInteractiveGuideline = false;
+    this.chartParameters.xAxis = "ME_DATE";
+    this.chartParameters.yAxis = ["ME_TEMP"];
+    this.chartParameters.xDataType = "timeDetail";
   }
 
-//   ngOnInit(): void {
-//     // Verificar si DEV_NAME está vacío al inicio
-//     if (!this.DEV_NAME && this.DEV_MAC) {
-//       // Si DEV_NAME no está definido, asignar el valor de DEV_MAC
-//       this.DEV_NAME = this.DEV_MAC;
-//     }
-//   }
+  createFilter(values: Array<{ attr, value }>): Expression {
+    let filters: Array<Expression> = [];
+    values.forEach(fil => {
+      if (fil.value) {
+        if (fil.attr === 'DEV_ID') {
+          filters.push(FilterExpressionUtils.buildExpressionEquals('DEV_ID', fil.value));
+        }
+        if (fil.attr === 'ME_DATE') {
+          filters.push(FilterExpressionUtils.buildExpressionMoreEqual('ME_DATE', fil.value.startDate._d.getTime()));
+        }
+        if (fil.attr === 'ME_DATE') {
+          filters.push(FilterExpressionUtils.buildExpressionLessEqual('ME_DATE', fil.value.endDate._d.getTime()));
+        }
+      }
+    });
 
-//   // Método que se puede llamar cuando el valor de DEV_MAC cambie (si es necesario)
-//   onDevMacChange(newMac: string): void {
-//     if (!this.DEV_NAME) {
-//       this.DEV_NAME = newMac;
-//     }
-//     this.DEV_MAC = newMac;
-//   }
+    if (filters.length > 0) {
+      return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
+    } else {
+      return null;
+    }
+  }
+
+  fillChart() {
+    this.dataArray = DataAdapterUtils.createDataAdapter(
+      this.chartParameters
+    ).adaptResult(this.measurementsTable.getDataArray());
+  }
 }
