817ad94c7ea3feeb9beb7f7a2bc6ccedcea81e89
Jorge González Fernández <j.gonzalezf@udc.es>
2025-01-14 12:06:30 +0100

    Fixed color rendering

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-column-renderer/devices-column-renderer.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-column-renderer/devices-column-renderer.component.ts
@@ -7,24 +7,22 @@ import { OBaseTableCellRenderer } from 'ontimize-web-ngx';
   styleUrls: ['./devices-column-renderer.component.css']
 })
 export class DevicesColumnRendererComponent extends OBaseTableCellRenderer implements OnInit {
-  @ViewChild('templateref', { read: TemplateRef, static: false }) public templateref: TemplateRef<any>;
-  obj = {
-    "color": "black"
-  }
-  isTimeElapsedGreaterThan15Min(cellVal):boolean{
-    const time: number = 15*60*1000;
-    const currentDate:  number = new Date ().getTime();
-    const dif: number = currentDate - cellVal;
-    return dif>time;
-  }
-  getCellData(value: any) {
-    console.log("llamada a getcellData")
-    if(this.isTimeElapsedGreaterThan15Min(value)){
-      this.obj["color"] = "red";
-    }else{
-      this.obj["color"] = "black";
-    }
 
+  @ViewChild('templateref', { read: TemplateRef, static: false }) public templateref: TemplateRef<any>;
+
+  isRed = true;
+
+  TIME: number = 15 * 60 * 1000;
+
+  calculateElapsedGreaterThan15Min(cellVal): void {
+    const currentDate: number = new Date().getTime();
+    const dif: number = currentDate - cellVal;
+    this.isRed = dif > this.TIME;
+  }
+
+  getCellData(value: any) {
+    this.calculateElapsedGreaterThan15Min(value);
     return value;
   }
+
 }
