9b4c426bad71810267cbd68c0fa305a5929c1b8d
IagoOlivares <iago2002oc@gmail.com>
2025-01-17 11:54:52 +0100

    cambio v8 singular

--- cd2024bfi1g1-model/src/main/resources/db/migration/V8__create_lots_tables.sql
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V8__create_lots_tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE public.lots (
-       lots_id serial NOT NULL,
-       lots_name varchar NOT NULL,
+       lot_id serial NOT NULL,
+       lot_name varchar NOT NULL,
        usr_id int4 NOT NULL,
-       CONSTRAINT lots_pk PRIMARY KEY (lots_id),
-       CONSTRAINT lots_usr_user_fk FOREIGN KEY (usr_id) REFERENCES public.usr_user(usr_id)
+       CONSTRAINT lot_pk PRIMARY KEY (lot_id),
+       CONSTRAINT lot_usr_user_fk FOREIGN KEY (usr_id) REFERENCES public.usr_user(usr_id)
 );
 
-ALTER TABLE public.devices ADD lots_id int4;
-ALTER TABLE public.devices ADD CONSTRAINT devices_lots_fk FOREIGN KEY (lots_id) REFERENCES public.lots(lots_id);
+ALTER TABLE public.devices ADD lot_id int4;
+ALTER TABLE public.devices ADD CONSTRAINT devices_lots_fk FOREIGN KEY (lot_id) REFERENCES public.lots(lot_id);
