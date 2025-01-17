c7f2c58ab4978e300eff13643d4f94060f2e3709
Gabriel <gabriel.lamela.ch@gmail.com>
2025-01-17 10:51:27 +0100

    Añadir borrado de columna usr_id en devices y creación de columna cli_id con su respectiva foreign key

--- cd2024bfi1g1-model/src/main/resources/db/migration/V8__create_clients_table.sql
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V8__create_clients_table.sql
@@ -10,3 +10,9 @@ VALUES(1, 'no_client');
 
 ALTER TABLE public.usr_user ADD cli_id int4 DEFAULT 1 NOT NULL;
 ALTER TABLE public.usr_user ADD CONSTRAINT usr_user_clients_fk FOREIGN KEY (cli_id) REFERENCES public.clients(cli_id);
+
+ALTER TABLE public.devices DROP CONSTRAINT devices_usr_user_fk;
+ALTER TABLE public.devices DROP COLUMN usr_id;
+
+ALTER TABLE public.devices ADD cli_id int4 DEFAULT 1 NOT NULL;
+ALTER TABLE public.devices ADD CONSTRAINT devices_clients_fk FOREIGN KEY (cli_id) REFERENCES public.clients(cli_id);
