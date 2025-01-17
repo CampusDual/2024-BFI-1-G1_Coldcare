39fb664d7289d95fb62a43f9833dc1bb69646900
Hugo <hugonvsg@gmail.com>
2025-01-17 12:44:52 +0100

    arreglado bug de carga de datos lenta v2

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/medidas/medidas-home/medidas-home.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/medidas/medidas-home/medidas-home.component.ts
@@ -1,17 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-medidas-home",
   templateUrl: "./medidas-home.component.html",
   styleUrls: ["./medidas-home.component.css"],
 })
-export class MedidasAdminComponent implements OnInit {
 
-  constructor() {}
-
-  ngOnInit(): void {
-    // Aquí iría la lógica de inicialización
-    console.log("Componente MedidasHome ha sido inicializado.");
-  }
-
-  }
+export class MedidasAdminComponent {}
