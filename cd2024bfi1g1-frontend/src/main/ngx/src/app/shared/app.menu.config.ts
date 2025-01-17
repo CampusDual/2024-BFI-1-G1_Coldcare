965613194f08edf6933a1cbf05f5ff3c587af3cf
Jorge González Fernández <j.gonzalezf@udc.es>
2025-01-17 11:03:17 +0100

    Hide useless features, Users and Roles from sidebar and Profile and Settings from toolbar

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -7,8 +7,8 @@ export const MENU_CONFIG: MenuRootItem[] = [
   {
     id: 'admin', name: 'ADMIN', tooltip: 'ADMIN', icon: 'admin_panel_settings',
     items: [
-      { id: 'roles', name: 'ROLES', tooltip: 'ROLES', route: '/main/admin/roles', icon: 'supervisor_account' },
-      { id: 'users', name: 'USERS', tooltip: 'USERS', route: '/main/admin/users', icon: 'person' },
+      // { id: 'roles', name: 'ROLES', tooltip: 'ROLES', route: '/main/admin/roles', icon: 'supervisor_account' },
+      // { id: 'users', name: 'USERS', tooltip: 'USERS', route: '/main/admin/users', icon: 'person' },
       { id: 'devices-without-users', name: 'DEVICE_ASSIGNMENT', tooltip: 'DevicesWithoutUsers', route: '/main/admin/devices-without-users', icon: 'developer_board' },
       { id: 'medidas', name: 'MEASUREMENTS', icon: 'thermostat', route: '/main/admin/medidas' },
     ]
