3a7d784687a5a3dc4ef7e6b909e090755676f99f
Gabriel <Gabrieldb050890@gmail.com>
2025-01-17 11:14:41 +0100

    corregido bug de retardo de carga de datos

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/medidas/medidas-home/medidas-home.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/medidas/medidas-home/medidas-home.component.ts
@@ -1,8 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-medidas-home",
   templateUrl: "./medidas-home.component.html",
   styleUrls: ["./medidas-home.component.css"],
 })
-export class MedidasAdminComponent {}
+export class MedidasAdminComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // Aquí iría la lógica de inicialización
+    console.log("Componente MedidasHome ha sido inicializado.");
+  }
+
+  }
