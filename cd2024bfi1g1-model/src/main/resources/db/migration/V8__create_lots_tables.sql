f9d81b3140784429dd3faa72804ae6b2dd67d94d
IagoOlivares <iago2002oc@gmail.com>
2025-01-21 11:47:47 +0100

    cambio nombres v8

--- cd2024bfi1g1-model/src/main/resources/db/migration/V8__create_lots_tables.sql
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V8__create_lots_tables.sql
@@ -6,20 +6,20 @@ CREATE TABLE public.lots (
        CONSTRAINT lot_usr_user_fk FOREIGN KEY (usr_id) REFERENCES public.usr_user(usr_id)
 );
 
-CREATE TABLE public.containerslots(
+CREATE TABLE public.containers_lots(
          cnt_id int4 NOT NULL,
          lot_id int4 NOT NULL,
-         CONSTRAINT containerslots_pk PRIMARY KEY (cnt_id, lot_id),
-         CONSTRAINT containerslots_containers_fk FOREIGN KEY (cnt_id) REFERENCES public.containers(cnt_id),
-         CONSTRAINT containerslots_lots_fk FOREIGN KEY (lot_id) REFERENCES public.lots(lot_id)
+         CONSTRAINT containers_lots_pk PRIMARY KEY (cnt_id, lot_id),
+         CONSTRAINT containers_lots_containers_fk FOREIGN KEY (cnt_id) REFERENCES public.containers(cnt_id),
+         CONSTRAINT containers_lots_lots_fk FOREIGN KEY (lot_id) REFERENCES public.lots(lot_id)
 );
 
-CREATE TABLE public.historicContainerLots(
+CREATE TABLE public.historic_container_lots(
          cnt_id int4 NOT NULL,
          lot_id int4 NOT NULL,
-         CONSTRAINT historicContainerLots_pk PRIMARY KEY (cnt_id, lot_id),
-         CONSTRAINT historicContainerLots_containerslots_fk FOREIGN KEY (cnt_id) REFERENCES public.containers(cnt_id),
-         CONSTRAINT historicContainerLots_lots_fk FOREIGN KEY (lot_id) REFERENCES public.lots(lot_id)
+         CONSTRAINT historic_container_lots_pk PRIMARY KEY (cnt_id, lot_id),
+         CONSTRAINT historic_container_lots_containers_lots_fk FOREIGN KEY (cnt_id) REFERENCES public.containers(cnt_id),
+         CONSTRAINT historic_container_lots_lots_fk FOREIGN KEY (lot_id) REFERENCES public.lots(lot_id)
 );
 
 ALTER TABLE public.measurements ADD lot_id int4;
