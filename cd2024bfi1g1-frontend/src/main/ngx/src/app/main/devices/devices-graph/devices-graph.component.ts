ed57cf68826924bc871b2ddf3df146f4f40d0a08
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2025-01-23 13:54:09 +0100

    Merge del html y ts de device-graph

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-graph/devices-graph.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/devices/devices-graph/devices-graph.component.ts
@@ -9,26 +9,33 @@ import { DataAdapterUtils, LineChartConfiguration, OChartComponent } from 'ontim
 })
 export class DevicesGraphComponent {
 
-  @ViewChild('measurementsTable', { static: false }) measurementsTable: OTableComponent;
+  //Grafica temperatura
+  @ViewChild('measurementsTemperatureTable', { static: false }) measurementsTemperatureTable: OTableComponent;
+  //Grafica humedad
+  @ViewChild('measurementsHumidityTable', { static: false }) measurementsHumidityTable: OTableComponent;
+
   @ViewChild('lineChartBasic', { static: false }) lineChart: OChartComponent;
 
-  dataArray: any = [];
+  dataArrayTemp: any = [];
+  dataArrayHum: any = [];
   chartData = [];
 
-  chartParameters: LineChartConfiguration;
+  chartParametersTemp: LineChartConfiguration;
+  chartParametersHum: LineChartConfiguration;
 
   colorScheme = {
     domain: ['#1464A5', '#eeeeee', '#c5c5c5']
   };
 
   constructor() {
-    this.chartParameters = new LineChartConfiguration();
-    this.chartParameters.isArea = [true];
-    this.chartParameters.interactive = false;
-    this.chartParameters.useInteractiveGuideline = false;
-    this.chartParameters.xAxis = "ME_DATE";
-    this.chartParameters.yAxis = ["ME_TEMP"];
-    this.chartParameters.xDataType = (d: number): string => {
+    //Temperatura
+    this.chartParametersTemp = new LineChartConfiguration();
+    this.chartParametersTemp.isArea = [true];
+    this.chartParametersTemp.interactive = true;
+    this.chartParametersTemp.useInteractiveGuideline = false;
+    this.chartParametersTemp.xAxis = "ME_DATE";
+    this.chartParametersTemp.yAxis = ["ME_TEMP"];
+    this.chartParametersTemp.xDataType = (d: number): string => {
       const date = new Date(d);
       const day = String(date.getDate()).padStart(2, '0');
       const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -37,10 +44,22 @@ export class DevicesGraphComponent {
 
       return `${day}/${month} ${hours}:${minutes}`;
     };
-    this.chartParameters['showTooltip'] = true;
+    this.chartParametersTemp['showTooltip'] = true;
+
+    //Humedad
+    this.chartParametersHum = new LineChartConfiguration();
+    this.chartParametersHum.isArea = [true];
+    this.chartParametersHum.interactive = false;
+    this.chartParametersHum.useInteractiveGuideline = false;
+    this.chartParametersHum.xAxis = "ME_DATE";
+    this.chartParametersHum.yAxis = ["ME_HUMIDITY"];
+    this.chartParametersHum.xDataType = "timeDetail";
   }
 
   createFilter(values: Array<{ attr, value }>): Expression {
+
+    console.log('Valores del filtro:', values);
+
     let filters: Array<Expression> = [];
     values.forEach(fil => {
       if (fil.value) {
@@ -56,6 +75,8 @@ export class DevicesGraphComponent {
       }
     });
 
+    console.log('Filtros generados:', filters);
+
     if (filters.length > 0) {
       return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
     } else {
@@ -63,10 +84,17 @@ export class DevicesGraphComponent {
     }
   }
 
-  fillChart() {
-    this.dataArray = DataAdapterUtils.createDataAdapter(
-      this.chartParameters
-    ).adaptResult(this.measurementsTable.getDataArray());
+  //LLenar tablas
+  fillChart(ev: any) {
+    console.log({ ev });
+
+    this.dataArrayTemp = DataAdapterUtils.createDataAdapter(
+      this.chartParametersTemp
+    ).adaptResult(ev);
+
+    this.dataArrayHum = DataAdapterUtils.createDataAdapter(
+      this.chartParametersHum
+    ).adaptResult(ev);
   }
 
 }
