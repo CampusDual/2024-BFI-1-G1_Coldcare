99b3e12037c2aa6c0eebe69bbc5e023428cb0222
Hugo <hugonvsg@gmail.com>
2025-01-23 09:45:10 +0100

    añadido filtro de nombre y fecha

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/medidas/medidas-home/medidas-home.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/medidas/medidas-home/medidas-home.component.ts
@@ -1,56 +1,75 @@
 import { Component } from "@angular/core";
-import { Expression, FilterExpressionUtils} from 'ontimize-web-ngx';
-import { LineChartConfiguration } from 'ontimize-web-ngx-charts';
+import { Expression, FilterExpressionUtils } from "ontimize-web-ngx";
 
 @Component({
   selector: "app-medidas-home",
   templateUrl: "./medidas-home.component.html",
   styleUrls: ["./medidas-home.component.css"],
 })
-
 export class MedidasAdminComponent {
- 
-    dataArray: any = [];
-    chartData = [];
+
+  colorScheme = {
+    domain: ["#1464A5", "#eeeeee", "#c5c5c5"],
+  };
+
+  constructor() {}
   
-    chartParameters: LineChartConfiguration;
-    
-    colorScheme = {
-      domain: ['#1464A5', '#eeeeee', '#c5c5c5']
-    };
-  
-    constructor() {
-      this.chartParameters = new LineChartConfiguration();
-      this.chartParameters.isArea = [true];
-      this.chartParameters.interactive = false;
-      this.chartParameters.useInteractiveGuideline = false;
-      this.chartParameters.xAxis = "ME_DATE";
-      this.chartParameters.yAxis = ["ME_TEMP"];
-      this.chartParameters.xDataType = "timeDetail";
-    }
-  
-    createFilter(values: Array<{ attr, value }>): Expression {
-      let filters: Array<Expression> = [];
-      values.forEach(fil => {
-        if (fil.value) {
-          if (fil.attr === 'DEV_ID') {
-            filters.push(FilterExpressionUtils.buildExpressionEquals('DEV_ID', fil.value));
-          }
-          if (fil.attr === 'ME_DATE') {
-            filters.push(FilterExpressionUtils.buildExpressionMoreEqual('ME_DATE', fil.value.startDate._d.getTime()));
-          }
-          if (fil.attr === 'ME_DATE') {
-            filters.push(FilterExpressionUtils.buildExpressionLessEqual('ME_DATE', fil.value.endDate._d.getTime()));
-          }
+  createFilter(values: Array<{ attr: string; value: any }>): Expression {
+    let filters: Array<Expression> = [];
+
+    values.forEach((fil) => {
+      if (fil.value) {
+        switch (fil.attr) {
+          case "DEV_ID":
+            // Filtrar por ID del dispositivo
+            filters.push(FilterExpressionUtils.buildExpressionEquals("DEV_ID", fil.value));
+            break;
+
+          case "ME_DATE":
+            // Filtrar por rango de fechas
+            if (fil.value.startDate) {
+              filters.push(
+                FilterExpressionUtils.buildExpressionMoreEqual(
+                  "ME_DATE",
+                  fil.value.startDate._d.getTime()
+                )
+              );
+            }
+            if (fil.value.endDate) {
+              filters.push(
+                FilterExpressionUtils.buildExpressionLessEqual(
+                  "ME_DATE",
+                  fil.value.endDate._d.getTime()
+                )
+              );
+            }
+            break;
+
+          case "DEV_NAME":
+            // Filtrar por nombre del dispositivo (insensible a mayúsculas/minúsculas)
+            filters.push(
+              FilterExpressionUtils.buildExpressionLike(
+                "DEV_NAME",
+                `%${fil.value}%`
+              )
+            );
+            break;
+
+          default:
+            console.warn(`Atributo desconocido: ${fil.attr}`);
+            break;
         }
-      });
-  
-      if (filters.length > 0) {
-        return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
-      } else {
-        return null;
       }
+    });
+
+    // Combinar los filtros con el operador AND si hay más de uno
+    if (filters.length > 0) {
+      return filters.reduce((exp1, exp2) =>
+        FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND)
+      );
+    } else {
+      return null; // Sin filtros aplicados
     }
-  
+  }
 
 }
