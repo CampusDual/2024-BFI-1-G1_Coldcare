b33c4853127714186772e87a5acb999d560e62ab
Kurosaki <pablobasantadominguez@gmail.com>
2025-01-24 13:07:24 +0000

    colores de error por fuera de los valores

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-lots-measurements/containers-lots-measurements.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-lots-measurements/containers-lots-measurements.component.ts
@@ -1,21 +1,16 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-containers-lots-measurements',
   templateUrl: './containers-lots-measurements.component.html',
   styleUrls: ['./containers-lots-measurements.component.css']
 })
-export class ContainersLotsMeasurementsComponent {
+export class ContainersLotsMeasurementsComponent{
 
-  getRowClass(rowData: any): string {
-    const temp = rowData.ME_TEMP;
-    const minTemp = rowData.MIN_TEMP;
-    const maxTemp = rowData.MAX_TEMP;
-
-    if (temp < minTemp || temp > maxTemp) {
-      return 'row-red'; 
-    }
-    return ''; 
+  public rowClass=(rowData: any, rowIndex: number) : string | string[] =>{
+    console.log({rowData});
+    return "error-row";
+    
   }
 
 }
--- cd2024bfi1g1-model/src/main/resources/db/migration/V11__create_min_max_temp.sql
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V11__create_min_max_temp.sql
@@ -1,2 +1,2 @@
-Alter TABLE public.lots ADD min_temp int4;
-Alter TABLE public.lots ADD max_temp int4;
+Alter TABLE public.lots ADD min_temp float8;
+Alter TABLE public.lots ADD max_temp float8;
