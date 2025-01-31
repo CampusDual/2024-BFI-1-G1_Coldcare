b63f872f850fa0576bec6cc7c142ec684203c654
Gabriel <gabriel.lamela.ch@gmail.com>
2025-01-31 14:12:21 +0100

    Crear filtro para lotes y contenedores en la vista de alerts-home, cambiar nombre de flyway V16 a V15

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/alerts/alerts-home/alerts-home.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/alerts/alerts-home/alerts-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Expression, FilterExpressionUtils, Util } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-alerts-home',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./alerts-home.component.css']
 })
 export class AlertsHomeComponent {
+
+  createFilter(values: Array<{ attr; value }>): Expression {
+    // Prepare simple expressions from the filter components values
+    let filters: Array<Expression> = [];
+    values.forEach(fil => {
+      if (Util.isDefined(fil.value)) {
+        const attributeMapping = {
+          lote: "lot_name",
+          contenedor: "cnt_name"
+        };
+
+        const fieldName = attributeMapping[fil.attr];
+        if (fieldName) {
+          filters.push(FilterExpressionUtils.buildExpressionLike(fieldName, fil.value));
+        }
+
+      }
+    });
+
+    // Build complex expression
+    if (filters.length > 0) {
+      return filters.reduce((exp1, exp2) =>
+        FilterExpressionUtils.buildComplexExpression(
+          exp1,
+          exp2,
+          FilterExpressionUtils.OP_AND
+        )
+      );
+    } else {
+      return null;
+    }
+  }
 
 }
