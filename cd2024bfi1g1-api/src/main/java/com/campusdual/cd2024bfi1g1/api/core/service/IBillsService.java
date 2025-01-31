403ea6f37e3f1637bcb6b95c442ead2e220bfebd
Gabriel <Gabrieldb050890@gmail.com>
2025-01-31 11:41:29 +0100

    cambios en backed

--- cd2024bfi1g1-api/src/main/java/com/campusdual/cd2024bfi1g1/api/core/service/IBillsService.java
+++ cd2024bfi1g1-api/src/main/java/com/campusdual/cd2024bfi1g1/api/core/service/IBillsService.java
@@ -1,4 +1,15 @@
 package com.campusdual.cd2024bfi1g1.api.core.service;
 
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+import java.util.List;
+import java.util.Map;
+
 public interface IBillsService {
+    EntityResult billsQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+    EntityResult billsInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+    EntityResult billsUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+    EntityResult billsDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
 }
+
--- cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/dao/BillsDao.java
+++ cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/dao/BillsDao.java
@@ -1,4 +1,24 @@
 package com.campusdual.cd2024bfi1g1.model.core.dao;
 
-public class BillDao {
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Repository("BillsDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/BillsDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+
+public class BillsDao extends OntimizeJdbcDaoSupport {
+
+    public static final String BIL_ID = "BIL_ID";
+    public static final String CMP_ID = "CMP_ID";
+    public static final String BIL_DATE = "BIL_DATE";
+    public static final String BIL_MEASUREMENTS = "BIL_MEASUREMENTS";
+    public static final String BIL_DEVICES = "BIL_DEVICES";
+    public static final String BIL_EXPENSE = "BIL_EXPENSE";
+    public static final List<String> COLUMNS = Arrays.asList(BIL_ID, CMP_ID, BIL_DATE, BIL_MEASUREMENTS, BIL_DEVICES,BIL_EXPENSE);
 }
