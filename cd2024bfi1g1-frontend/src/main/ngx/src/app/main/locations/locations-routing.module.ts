7e2ed3e9f8955bd71ec91a51e21b7628e6bad9f2
Gabriel <gabriel.lamela.ch@gmail.com>
2025-01-31 12:00:19 +0100

    Crear locations-new component y routing. Añadir traducciones a diccionarios. Cambiar nombre de V14 a V15 del flyway

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/locations/locations-routing.module.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/locations/locations-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LocationsHomeComponent } from './locations-home/locations-home.component';
 import { LocationsDetailComponent } from './locations-detail/locations-detail.component';
+import { LocationsNewComponent } from './locations-new/locations-new.component';
 
 const routes: Routes = [
   { path: '', component: LocationsHomeComponent },
+  { path: 'new', component: LocationsNewComponent },
   { path: ':LOC_ID', component: LocationsDetailComponent },
 ];
 
--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/locations/locations.module.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/locations/locations.module.ts
@@ -5,11 +5,13 @@ import { SharedModule } from '../../shared/shared.module';
 import { LocationsRoutingModule } from './locations-routing.module';
 import { LocationsHomeComponent } from './locations-home/locations-home.component';
 import { LocationsDetailComponent } from './locations-detail/locations-detail.component';
+import { LocationsNewComponent } from './locations-new/locations-new.component';
 
 @NgModule({
   declarations: [
     LocationsHomeComponent,
-    LocationsDetailComponent
+    LocationsDetailComponent,
+    LocationsNewComponent
   ],
 
   imports: [
