04a79fbb67c2b8fffc115ee0f982293ec0b63c0f
costasgc <adriancostasgarcia1998@gmail.com>
2025-01-31 09:45:50 +0100

    Created vehicles table

---
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V14__create_vehicles_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.vehicles(
+    vhc_id serial NOT NULL,
+    vhc_plate varchar NOT NULL,
+    cmp_id int4,
+    CONSTRAINT vhc_pk PRIMARY KEY (vhc_id),
+
+);
+
+ALTER table public.vehicles ADD CONSTRAINT companies_vehicles_fk FOREIGN KEY (cmp_id) REFERENCES public.companies(cmp_id);
