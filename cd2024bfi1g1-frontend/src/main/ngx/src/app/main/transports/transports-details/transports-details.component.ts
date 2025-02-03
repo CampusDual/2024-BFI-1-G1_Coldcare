f8b8810a51b5766428e40edcf8e672dec9e39a1f
IagoOlivares <iago2002oc@gmail.com>
2025-02-03 14:56:32 +0100

    creacion home, new de transportes

---
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/transports/transports-details/transports-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transports-details',
+  templateUrl: './transports-details.component.html',
+  styleUrls: ['./transports-details.component.css']
+})
+export class TransportsDetailsComponent {
+
+}
