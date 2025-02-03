34eb86ab4723ab84807d10d91e8033cbc1198fb6
KurosakiVll <pablobasantadominguez@gmail.com>
2025-02-03 10:56:31 +0000

    changed migration script V14toV16

--- cd2024bfi1g1-model/src/main/resources/db/migration/V14__create_vehicles_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.vehicles(
-    vhc_id serial NOT NULL,
-    vhc_plate varchar NOT NULL,
-    cmp_id int4,
-    CONSTRAINT vhc_pk PRIMARY KEY (vhc_id)
-
-);
-
-ALTER table public.vehicles ADD CONSTRAINT companies_vehicles_fk FOREIGN KEY (cmp_id) REFERENCES public.companies(cmp_id);
