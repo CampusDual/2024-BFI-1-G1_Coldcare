b842e62c9d10d742dc368b6fc8942166640e22dc
IagoOlivares <iago2002oc@gmail.com>
2025-02-05 15:06:06 +0100

    campos obligatorios, boton eliminar vehiculo, logica cambio de movilidad

--- cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/service/ContainersService.java
+++ cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/service/ContainersService.java
@@ -12,6 +12,7 @@ import com.ontimize.jee.common.services.user.UserInformation;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -98,12 +99,35 @@ public class ContainersService implements IContainersService {
                     "Ya existe un contenedor con ese nombre");
         }
 
+        if (attrMap.containsKey("CNT_MOBILITY")) {
+            if (isContainerAssigned(keyMap.get("CNT_ID"))) {
+                EntityResult res = new EntityResultMapImpl();
+                res.setCode(EntityResult.OPERATION_WRONG);
+                res.setMessage("CONTAINER_MOBILITY_ERROR");
+                return res;
+            }else{
+                return this.daoHelper.update(this.containersDao, attrMap, keyMap);
+            }
+        }
+
         return this.daoHelper.update(this.containersDao, attrMap, keyMap);
     }
 
     @Override
     public EntityResult containersDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
         return this.daoHelper.delete(this.containersDao, keyMap);
+    }
+
+    public boolean isContainerAssigned(Object cntId) {
+        Map<String, Object> keyMap = new HashMap<>();
+        keyMap.put("CNT_ID", cntId);
+
+        List<String> attrList = new ArrayList<>();
+        attrList.add("CNT_ID");
+
+        EntityResult result = this.daoHelper.query(this.containersDao, keyMap, attrList, "check_cnt_id");
+
+        return result != null && result.calculateRecordNumber() > 0;
     }
 
 }
