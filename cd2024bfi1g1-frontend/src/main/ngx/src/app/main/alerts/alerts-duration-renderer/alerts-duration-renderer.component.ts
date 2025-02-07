07e18865a57b3b995c863d389a684a0cb6c46f3f
Gabriel <gabriel.lamela.ch@gmail.com>
2025-02-07 15:11:40 +0100

    Hacer el cálculo de duración de la alerta en front con las fechas de inicio y fin de la tabla alerts
    Introducir nuevas traducciones
    Cambiar query en AlertsDao para que no devuelva el cálculo de duración

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/alerts/alerts-duration-renderer/alerts-duration-renderer.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/alerts/alerts-duration-renderer/alerts-duration-renderer.component.ts
@@ -9,8 +9,17 @@ import { OBaseTableCellRenderer } from 'ontimize-web-ngx';
 export class AlertsDurationRendererComponent extends OBaseTableCellRenderer implements OnInit {
   @ViewChild('templateref', { read: TemplateRef, static: false }) public templateref: TemplateRef<any>;
 
-  calculateElapsedTime(cellVal): String {
-    const totalSeconds = Math.ceil(cellVal);
+  getCellData(rowvalue: any): string {
+
+    let totalSeconds: number;
+
+    if (rowvalue["ALT_DATE_END"] === undefined) {
+      const now = new Date;
+      totalSeconds = Math.floor((now.getTime() - new Date(rowvalue["ALT_DATE_INIT"]).getTime()) / 1000);
+    } else {
+      totalSeconds = Math.floor((rowvalue["ALT_DATE_END"] - rowvalue["ALT_DATE_INIT"]) / 1000);
+    }
+
     const days = Math.floor(totalSeconds / (24 * 3600));
     const hours = Math.floor((totalSeconds % (24 * 3600)) / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
