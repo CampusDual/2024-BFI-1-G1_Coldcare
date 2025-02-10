151d46a64438eb5cf178fda7ee47d1e7beade01c
Hugo <hugonvsg@gmail.com>
2025-02-10 09:57:32 +0100

    prueba

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/lots/lots-new/lots-new.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/lots/lots-new/lots-new.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, NgZone } from '@angular/core';
+import { OFormComponent, OntimizeService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-lots-new',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class LotsNewComponent {
 
+  @ViewChild('lotsFormInsert', { static: false }) lotsFormInsert!: OFormComponent;
+
+  constructor(private ontimizeService: OntimizeService, private ngZone: NgZone) {}
+
+  onProductChange(event: any) {
+    if (!event || !event.detail?.newValue) return;
+
+    const selectedProduct = event.detail.newValue;
+
+    // Ejecutar cambios fuera de la detección de cambios de Angular
+    this.ngZone.run(() => {
+      this.lotsFormInsert.setFieldValue('MIN_TEMP', selectedProduct.PRO_MIN_TEMP ?? '');
+      this.lotsFormInsert.setFieldValue('MAX_TEMP', selectedProduct.PRO_MAX_TEMP ?? '');
+    });
+  }
 }
