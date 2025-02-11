26de0414c22384223377809de8ebb8bcae719a65
Gabriel <Gabrieldb050890@gmail.com>
2025-02-11 11:02:23 +0100

    arreglada base de datos y parte de visualizacion de productos en historial de lotes en containers

--- cd2024bfi1g1-model/src/main/resources/db/migration/V20__create_product_table.sql
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V20__create_product_table.sql
@@ -9,5 +9,5 @@ ALTER TABLE public.lots ADD pro_id int4 NULL;
 ALTER TABLE public.lots ADD CONSTRAINT usr_lots_fk FOREIGN KEY (pro_id) REFERENCES public.products(pro_id);
 
 UPDATE public.usr_role
-SET rol_json_client_permission='{ "menu": [{ "attr": "contenedores", "visible": false, "enabled": false }, { "attr": "devices", "visible": false, "enabled": false }, { "attr": "lots", "visible": false, "enabled": false }, { "attr": "locations", "visible": false, "enabled": false }, { "attr": "products", "visible": false, "enabled": false } ] }'
+SET rol_json_client_permission='{ "menu": [{ "attr": "contenedores", "visible": false, "enabled": false }, { "attr": "devices", "visible": false, "enabled": false }, { "attr": "lots", "visible": false, "enabled": false },{ "attr": "locations", "visible": false, "enabled": false }, { "attr": "vehicles", "visible": false, "enabled": false }, { "attr": "transports", "visible": false, "enabled": false },{ "attr": "products", "visible": false, "enabled": false } ] }'
 WHERE rol_id=1;
