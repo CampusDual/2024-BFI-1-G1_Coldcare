517a0fbde1f829c71d824feb557297a75d09e2cb
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2025-02-14 14:41:12 +0100

    Primer intento de cargar puntos  en mapa

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/transports/transports-map/transports-map.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/transports/transports-map/transports-map.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { OMapComponent } from 'ontimize-web-ngx-map';
+import * as L from 'leaflet';
+import { OTableComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-transports-map',
   templateUrl: './transports-map.component.html',
   styleUrls: ['./transports-map.component.css']
 })
-export class TransportsMapComponent {
+export class TransportsMapComponent implements AfterViewInit {
 
+  @ViewChild('oMapRouting', { static: false }) oMap!: OMapComponent;
+
+  dataArrayCoordinates: any = [];
+
+
+  ngAfterViewInit() {
+    this.cargarCoordenadas();
+  }
+
+  cargarCoordenadas() {
+    try {
+      // Cargar las coordenadas de la tabla
+
+
+      
+
+      // Obtener el mapa de Ontimize
+      const map = this.oMap.getLMap();
+
+      // Agregar marcadores al mapa
+      this.dataArrayCoordinates.forEach(punto => {
+        const marker = L.marker([punto["TC_LATITUDE"], punto["TC_LONGITUDE"]]);
+        marker.addTo(map);
+      });
+      
+
+    } catch (error) {
+      console.error('Error al cargar coordenadas:', error);
+    }
+  }
+
+  fillChart(ev: any) {
+
+    const data = ev.map((row) => {
+      return {
+        ["TC_LATITUDE"]: row.TC_LATITUDE,
+        ["TC_LONGITUDE"]: row.TC_LONGITUDE
+      }
+    });
+    this.dataArrayCoordinates = data;
+  }
 }
