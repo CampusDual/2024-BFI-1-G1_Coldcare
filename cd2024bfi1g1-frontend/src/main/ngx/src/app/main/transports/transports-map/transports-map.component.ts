1e33c6707ae6a813c9432acd9fa7b629947501e5
Gabriel <gabriel.lamela.ch@gmail.com>
2025-02-14 11:07:26 +0100

    Instalar ontimize-web-ngx-map, crear componente transports-map

---
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/transports/transports-map/transports-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transports-map',
+  templateUrl: './transports-map.component.html',
+  styleUrls: ['./transports-map.component.css']
+})
+export class TransportsMapComponent {
+
+}
