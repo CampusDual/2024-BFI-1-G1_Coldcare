3473184865a09cdbd134a135591b0cf3bb783f09
IagoOlivares <iago2002oc@gmail.com>
2025-02-17 09:26:32 +0100

    vehicle validation changes

--- cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/service/VehiclesService.java
+++ cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/service/VehiclesService.java
@@ -39,42 +39,33 @@ public class VehiclesService implements IVehiclesService {
         return this.daoHelper.query(this.vehiclesDao, keyMap, attrList);
     }
 
-    private boolean changeVehiclePlate(Integer cmpId, Map<String, Object> attrMap) {
-        // Obtener la lista de vehiculos del usuario
-        Map<String, Object> keyMap = new HashMap<>();
-        keyMap.put(VehiclesDao.VHC_ID, cmpId);
-        List<String> attrList = new ArrayList<>();
-        attrList.add(VehiclesDao.VHC_PLATE);
-        EntityResult existingVehicles = this.vehiclesQuery(keyMap, attrList);
-
-        // Verificar si el nombre del nuevo vehiculo ya existe
+    private boolean isPlateAlreadyAssigned(Integer cmpId, Map<String, Object> attrMap) {
         String newVehiclePlate = (String) attrMap.get(VehiclesDao.VHC_PLATE);
-
-        if (newVehiclePlate == null) {
+        if (newVehiclePlate == null || newVehiclePlate.trim().isEmpty()) {
             return false;
         }
-        String trimmedContainerPlate = newVehiclePlate.trim();
+        newVehiclePlate = newVehiclePlate.trim();
 
-        List<String> existingVehiclePlates = new ArrayList<>();
-        for (int i = 0; i < existingVehicles.calculateRecordNumber(); i++) {
-            existingVehiclePlates.add((String) existingVehicles.getRecordValues(i).get(VehiclesDao.VHC_PLATE));
-        }
-        for (Object vehiclePlate : existingVehiclePlates) {
-            if (trimmedContainerPlate.equals(vehiclePlate)) {
-                return true;
-            }
-        }
+        Map<String, Object> keyMap = new HashMap<>();
+        keyMap.put(VehiclesDao.CMP_ID, cmpId);
+        keyMap.put(VehiclesDao.VHC_PLATE, newVehiclePlate);
 
-        return false;
+        List<String> attrList = new ArrayList<>();
+        attrList.add(VehiclesDao.VHC_PLATE);
+        EntityResult result = this.daoHelper.query(this.vehiclesDao, keyMap, attrList);
+
+        return result.calculateRecordNumber() > 0;
     }
 
     @Override
     public EntityResult vehiclesInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
         Integer cmpId = UserAndRoleService.getUserCompanyId(this.daoHelper, this.userDao);
         attrMap.put(VehiclesDao.CMP_ID, cmpId);
-        if (changeVehiclePlate(cmpId, attrMap)) {
-            return new EntityResultMapImpl(EntityResult.OPERATION_WRONG, EntityResult.NODATA_RESULT,
-                    "Ya existe un vehiculo con ese nombre");
+        if (isPlateAlreadyAssigned(cmpId, attrMap)) {
+            EntityResult res = new EntityResultMapImpl();
+            res.setCode(EntityResult.OPERATION_WRONG);
+            res.setMessage("VEHICLE_DUPLICATE_INSERT");
+            return res;
         }
         return this.daoHelper.insert(this.vehiclesDao, attrMap);
     }
@@ -83,9 +74,11 @@ public class VehiclesService implements IVehiclesService {
     public EntityResult vehiclesUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
         Integer cmpId = UserAndRoleService.getUserCompanyId(this.daoHelper, this.userDao);
         attrMap.put(VehiclesDao.CMP_ID, cmpId);
-        if (changeVehiclePlate(cmpId, attrMap)) {
-            return new EntityResultMapImpl(EntityResult.OPERATION_WRONG, EntityResult.NODATA_RESULT,
-                    "Ya existe un vehiculo con ese nombre");
+        if (isPlateAlreadyAssigned(cmpId, attrMap)) {
+            EntityResult res = new EntityResultMapImpl();
+            res.setCode(EntityResult.OPERATION_WRONG);
+            res.setMessage("VEHICLE_DUPLICATE_INSERT");
+            return res;
         }
         return this.daoHelper.update(this.vehiclesDao, attrMap, keyMap);
     }
