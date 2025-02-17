05825251107856a681511d7091397e554c9b8284
Gabriel <Gabrieldb050890@gmail.com>
2025-02-17 11:59:03 +0100

    boton en tabla y checkbox

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/alerts/alerts-home/alerts-home.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/alerts/alerts-home/alerts-home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { Expression, FilterExpressionUtils, Util } from 'ontimize-web-ngx';
+import { identifierName } from '@angular/compiler';
+import { Component, ViewChild } from '@angular/core';
+import { Expression, FilterExpressionUtils, OTableComponent, Util } from 'ontimize-web-ngx';
+import { identity } from 'rxjs';
 
 @Component({
   selector: 'app-alerts-home',
@@ -7,6 +9,7 @@ import { Expression, FilterExpressionUtils, Util } from 'ontimize-web-ngx';
   styleUrls: ['./alerts-home.component.css']
 })
 export class AlertsHomeComponent {
+  @ViewChild("alertsTable") alertsTable: OTableComponent;
 
   createFilter(values: Array<{ attr; value }>): Expression {
     // Prepare simple expressions from the filter components values
@@ -55,5 +58,39 @@ export class AlertsHomeComponent {
     console.log(arg);
   }
 
+  selectionRow(event) {
+
+    console.log(this.alertsTable.getSelectedItems());
+  }
+  /*
+    selectionRow() {
+  
+    /*  const data = this.alertsTable.getDataArray();
+      for (let row of data) {
+        if (row.getValue("ALT_STATE")) {
+          console.log(row.getValue("ALT_STATE"));
+        }
+        console.log(row);
+      }
+  
+      // Verificar si Ontimize proporciona un método para obtener las filas seleccionadas
+      const selectedRows = this.alertsTable.selectAll;
+  
+      console.log("a", this.alertsTable.selectAll);
+      console.log("b", this.alertsTable.selectAllCheckbox);
+      console.log("c", this.alertsTable.selectedRow);
+      console.log(selectedRows);
+      // Si 'selectedRows' es una propiedad de filas seleccionadas, utilizamos directamente
+      if (Array.isArray(selectedRows) && selectedRows.length > 0) {
+        console.log('Filas seleccionadas:', selectedRows);
+        selectedRows.forEach(row => {
+          console.log('Guardando estado para fila:', row);
+          // Aquí puedes realizar cualquier acción, como guardar el estado de la fila
+        });
+      } else {
+        console.log('No hay filas seleccionadas');
+      }
+    }
+  */
 
 }
