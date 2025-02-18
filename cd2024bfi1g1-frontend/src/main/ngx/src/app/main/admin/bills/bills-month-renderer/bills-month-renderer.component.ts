fe420180eafa13e78abcef97ec4e11a06e3c2f3a
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2025-02-18 09:27:15 +0100

    Transformar numero de mes a nombre del mes y traducciones

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/bills/bills-month-renderer/bills-month-renderer.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/bills/bills-month-renderer/bills-month-renderer.component.ts
@@ -1,42 +1,53 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, Injector, TemplateRef, ViewChild } from '@angular/core';
+import { OBaseTableCellRenderer, OTranslateService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-bills-month-renderer',
   templateUrl: './bills-month-renderer.component.html',
   styleUrls: ['./bills-month-renderer.component.css']
 })
-export class BillsMonthRendererComponent {
+export class BillsMonthRendererComponent extends OBaseTableCellRenderer {
   @ViewChild('templateref', { read: TemplateRef, static: false }) public templateref: TemplateRef<any>;
 
-  getCellData(value: any) {
-    switch (value) {
-      case 1:
-        return "Enero";
-      case 2:
-        return "Febrero";
-      case 3:
-        return "Marzo";
-      case 4:
-        return "Abril";
-      case 5:
-        return "Mayo";
-      case 6:
-        return "Junio";
-      case 7:
-        return "Julio";
-      case 8:
-        return "Agosto";
-      case 9:
-        return "Septiembre";
-      case 10:
-        return "Octubre";
-      case 11:
-        return "Noviembre";
-      case 12:
-        return "Diciembre";
-      default:
-        return "Valor inválido";
-    }
+  translator: OTranslateService;
+
+  constructor(protected injector: Injector) {
+    super(injector); // Pasamos el injector a la clase padre
+    this.translator = this.injector.get(OTranslateService); // Obtenemos el servicio de traducción
   }
 
+  getCellData(value: any): string {
+    if (!this.translator) {
+      return 'ERROR';
+    }
+
+    switch (value) {
+      case 1:
+        return this.translator.get('JANUARY');
+      case 2:
+        return this.translator.get('FEBRUARY');
+      case 3:
+        return this.translator.get('MARCH');
+      case 4:
+        return this.translator.get('APRIL');
+      case 5:
+        return this.translator.get('MAY');
+      case 6:
+        return this.translator.get('JUNE');
+      case 7:
+        return this.translator.get('JULY');
+      case 8:
+        return this.translator.get('AUGUST');
+      case 9:
+        return this.translator.get('SEPTEMBER');
+      case 10:
+        return this.translator.get('OCTOBER');
+      case 11:
+        return this.translator.get('NOVEMBER');
+      case 12:
+        return this.translator.get('DECEMBER');
+      default:
+        return 'ERROR';
+    }
+  }
 }
