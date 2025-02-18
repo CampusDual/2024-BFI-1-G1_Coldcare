28ccc6855bc474e29ab2a722632e4ea4bd897891
Gabriel <gabriel.lamela.ch@gmail.com>
2025-02-18 10:46:20 +0100

    Añadir cmp_id a la containersOfLotQuery

--- cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/service/ContainersLotsService.java
+++ cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/service/ContainersLotsService.java
@@ -2,10 +2,11 @@ package com.campusdual.cd2024bfi1g1.model.core.service;
 
 import com.campusdual.cd2024bfi1g1.api.core.service.IContainersLotsService;
 import com.campusdual.cd2024bfi1g1.model.core.dao.ContainersLotsDao;
+import com.campusdual.cd2024bfi1g1.model.core.dao.DevicesDao;
+import com.campusdual.cd2024bfi1g1.model.core.dao.UserDao;
 import com.campusdual.cd2024bfi1g1.model.core.util.Util;
 import com.ontimize.jee.common.db.SQLStatementBuilder;
 import com.ontimize.jee.common.dto.EntityResult;
-import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,8 @@ public class ContainersLotsService implements IContainersLotsService {
     private ContainersLotsDao containersLotsDao;
     @Autowired
     private DefaultOntimizeDaoHelper daoHelper;
+    @Autowired
+    private UserDao userDao;
 
 
     @Override
@@ -77,6 +80,10 @@ public class ContainersLotsService implements IContainersLotsService {
 
     @Override
     public EntityResult containersOfLotQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
+
+        Integer cmpId = UserAndRoleService.getUserCompanyId(this.daoHelper, this.userDao);
+        keyMap.put(DevicesDao.CMP_ID, cmpId);
+
         return this.daoHelper.query(this.containersLotsDao, keyMap, attrList, "containers_of_lot");
     }
 
