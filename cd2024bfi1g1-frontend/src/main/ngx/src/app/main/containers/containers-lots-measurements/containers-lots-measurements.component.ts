e0c91d62c764ccdd93534eb65154d9f1992cef60
Jorge González Fernández <j.gonzalezf@udc.es>
2025-02-18 11:57:04 +0100

    Bug fixed detail-mode navigation

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-lots-measurements/containers-lots-measurements.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-lots-measurements/containers-lots-measurements.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { OTableComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-containers-lots-measurements',
@@ -8,9 +7,6 @@ import { OTableComponent } from 'ontimize-web-ngx';
   styleUrls: ['./containers-lots-measurements.component.css']
 })
 export class ContainersLotsMeasurementsComponent {
-
-  @ViewChild('containerLotOriginTable', { static: false }) clOriginTable: OTableComponent;
-  @ViewChild('containerLotDestinyTable', { static: false }) clDestinyTable: OTableComponent;
 
   constructor(
     private router: Router
@@ -28,21 +24,13 @@ export class ContainersLotsMeasurementsComponent {
     return '';
   }
 
-  public openContainerLotOriginDetail() {
-    let selected = this.clOriginTable.getSelectedItems();
-    this.redirect(selected);
-  }
-
-  public openContainerLotDestinyDetail() {
-    let selected = this.clDestinyTable.getSelectedItems();
-    this.redirect(selected);
+  public openContainersLotsDetail(selected: any) {
+    this.redirect(selected.row);
   }
 
   private redirect(selected: any) {
-    if (selected.length === 1) {
-      let containerId = selected[0]['CNT_ID'];
-      let clId = selected[0]['CL_ID'];
-      this.router.navigate(['main/containers/' + containerId + '/' + clId], { queryParams: { isdetail: true } });
+    if (selected && selected.CNT_ID && selected.CL_ID) {
+      this.router.navigate(['main', 'containers', selected.CNT_ID, selected.CL_ID], { queryParams: { isdetail: true } });
     }
   }
 
