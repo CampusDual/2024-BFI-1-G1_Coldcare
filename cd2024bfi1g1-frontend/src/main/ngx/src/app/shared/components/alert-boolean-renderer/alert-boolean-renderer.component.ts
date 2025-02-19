ea196fa0140433b8a620cc6caad379677920e196
Gabriel <gabriel.lamela.ch@gmail.com>
2025-02-19 10:39:29 +0100

    Modify appearance of alert and mobility columns in containers-home
    Modify translations for bills and transports views

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/components/alert-boolean-renderer/alert-boolean-renderer.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/components/alert-boolean-renderer/alert-boolean-renderer.component.ts
@@ -1,16 +1,13 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { OBaseTableCellRenderer } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'alert-boolean-renderer',
   templateUrl: './alert-boolean-renderer.component.html',
   styleUrls: ['./alert-boolean-renderer.component.css']
 })
-export class AlertBooleanRendererComponent {
+export class AlertBooleanRendererComponent extends OBaseTableCellRenderer {
 
   @ViewChild('templateref', { read: TemplateRef }) public templateref: TemplateRef<any>;
-
-  isDateInRange(): boolean {
-    return (true);
-  }
 
 }
