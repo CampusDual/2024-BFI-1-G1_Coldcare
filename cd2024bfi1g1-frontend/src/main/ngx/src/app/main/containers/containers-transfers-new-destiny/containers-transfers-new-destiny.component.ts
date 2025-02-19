e6bb098c07fc3ce059d60908c68fc29ed019b62c
Hugo <hugonvsg@gmail.com>
2025-02-19 13:41:34 +0100

    Created container selecting table

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-transfers-new-destiny/containers-transfers-new-destiny.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-transfers-new-destiny/containers-transfers-new-destiny.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { OIntegerInputComponent } from 'ontimize-web-ngx';
 
-@Component({
-  selector: 'app-containers-transfers-new-destiny',
-  templateUrl: './containers-transfers-new-destiny.component.html',
-  styleUrls: ['./containers-transfers-new-destiny.component.css']
-})
+  @Component({
+    selector: 'app-containers-transfers-new-destiny',
+    templateUrl: './containers-transfers-new-destiny.component.html',
+    styleUrls: ['./containers-transfers-new-destiny.component.css']
+  })
+
 export class ContainersTransfersNewDestinyComponent {
 
+  @ViewChild('inputDestiny', { static: false }) inputDestiny!: OIntegerInputComponent;
+
+  constructor() {}
+
+  onRowClick(event: any): void {
+
+    if (event.row.CL_ID) {
+      this.inputDestiny.setValue(event.row.CL_ID);
+    }
+  }
 }
