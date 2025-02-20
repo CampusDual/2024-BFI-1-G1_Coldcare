3a8de46491d9793f0d325b085e1aa267e1bc8d04
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2025-02-20 14:09:34 +0100

    Comprobacion de alerta activa

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/alerts/alerts.module.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/alerts/alerts.module.ts
@@ -5,13 +5,11 @@ import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { AlertsRoutingModule } from './alerts-routing.module';
 import { AlertsHomeComponent } from './alerts-home/alerts-home.component';
 import { AlertsDurationRendererComponent } from './alerts-duration-renderer/alerts-duration-renderer.component';
-import { AlertsIsCurrentRenderComponent } from './alerts-is-current-render/alerts-is-current-render.component';
 
 @NgModule({
   declarations: [
     AlertsHomeComponent,
-    AlertsDurationRendererComponent,
-    AlertsIsCurrentRenderComponent
+    AlertsDurationRendererComponent
   ],
   imports: [
     CommonModule,
