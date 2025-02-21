ab4165c8cb31419bdb75e5c4e97a906f5bccef37
Alex Domínguez <doalex7@gmail.com>
2025-02-21 13:15:12 +0100

    Fixed prices insert

--- cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/service/PricingService.java
+++ cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/service/PricingService.java
@@ -8,10 +8,12 @@ import com.campusdual.cd2024bfi1g1.model.core.util.Util;
 import com.ontimize.jee.common.db.AdvancedEntity;
 import com.ontimize.jee.common.db.SQLStatementBuilder;
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -36,29 +38,25 @@ public class PricingService implements IPricingService {
 
     @Override
     public EntityResult pricingInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
-        EntityResult toRet =this.daoHelper.insert(this.pricingDao, attrMap);
-
-        Map<String, Object> date = new HashMap<String, Object>();
-        date.put(PricingDao.PP_END, attrMap.get(PricingDao.PP_START));
-
-        Map<String, Object> keyMap = new HashMap<String, Object>();
-        keyMap.put(PricingDao.PP_ID,toRet.getRecordValues(0));
-        List<String> column = List.of(PricingDao.PLN_ID);
-        EntityResult planId = this.daoHelper.query(this.pricingDao, keyMap, column);
-
-        Map<String, Object> planIdMap = new HashMap<String, Object>();
-        planIdMap.put(PricingDao.PLN_ID, planId.getRecordValues(0));
-
-        List<String> filter = List.of(PricingDao.PP_ID);
-        EntityResult priceId = this.daoHelper.query(this.pricingDao, planIdMap, filter, "last_prices");
-        if(priceId.isEmpty() || priceId.isWrong()) {
-            return toRet;
+        try {
+            List<String> column = List.of(PricingDao.PP_ID);
+            Map<String, Object> plnId = new HashMap<>();
+            plnId.put(PricingDao.PLN_ID, attrMap.get(PricingDao.PLN_ID));
+            EntityResult priceId = this.daoHelper.query(this.pricingDao, plnId, column, "last_prices");
+            if(!priceId.isEmpty() && !priceId.isWrong()) {
+                Map<String, Object> date = new HashMap<String, Object>();
+                date.put(PricingDao.PP_END, attrMap.get(PricingDao.PP_START));
+                this.daoHelper.update(this.pricingDao,date,priceId.getRecordValues(0));
+            }
+            return this.daoHelper.insert(this.pricingDao, attrMap);
+        } catch (DataIntegrityViolationException e) {
+            EntityResult res = new EntityResultMapImpl();
+            res.setCode(EntityResult.OPERATION_WRONG);
+            res.setMessage("PRICES_INSERT_ERROR");
+            return res;
         }
-        Map<String, Object> priceMap = new HashMap<String, Object>();
-        priceMap.put(PricingDao.PP_ID, priceId.getRecordValues(0));
-        this.daoHelper.update(this.pricingDao,date,priceMap);
 
-        return toRet;
+
     }
 
     @Override
