2d6219e8c477685e59ebb699c84de320eb0e1518
Gabriel <Gabrieldb050890@gmail.com>
2025-02-21 12:40:52 +0100

    added visited variable in local store so that when redirecting does not change the page only

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/main.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/main.component.ts
@@ -9,16 +9,20 @@ import { OntimizeService } from 'ontimize-web-ngx';
 })
 export class MainComponent implements OnInit {
 
-  constructor(private router: Router, private oService: OntimizeService) {}
+  constructor(private router: Router, private oService: OntimizeService) { }
 
   ngOnInit() {
-    
+
+    const visited = localStorage.getItem('visited');
+
     this.oService.configureService(this.oService.getDefaultServiceConfiguration("users"));
-    this.oService.query({}, ["ROL_ID", "ROL_NAME"],"myRole").subscribe(ress => {
-      
-      if(ress['data'][0]['ROL_NAME'] == "admin"){
-        this.router.navigate(['main','admin','devices-without-users'],{});
+    this.oService.query({}, ["ROL_ID", "ROL_NAME"], "myRole").subscribe(ress => {
+
+      if (ress['data'][0]['ROL_NAME'] == "admin" && !visited) {
+
+        localStorage.setItem('visited', 'true');
+        this.router.navigate(['main', 'admin', 'devices-without-users']);
       }
-    }) 
+    });
   }
 }
