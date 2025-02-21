59b8e34de9889cddca1ff4f05540c98f79d512fd
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2025-02-21 14:24:47 +0100

    Modificacion del flyway y el menu lateral

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -2,11 +2,11 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'devices', name: 'DEVICES', icon: 'router', route: '/main/devices' },
-  { id: 'contenedores', name: 'CONTAINERS', icon: 'takeout_dining', route: '/main/containers' },
-  { id: 'lots', name: 'LOTS', icon: 'archive', route: '/main/lots' },
-  { id: 'locations', name: 'LOCATIONS', icon: 'location_on', route: '/main/locations' },
-  { id: 'vehicles', name: 'VEHICLES', tooltip: 'VEHICLES', icon: 'directions_car', route: '/main/vehicles' },
-  { id: 'transports', name: 'TRANSPORTS', tooltip: 'TRANSPORTS', icon:'local_shipping', route: '/main/transports' },
+  { id: 'contenedores', name: 'CONTAINERS', icon: 'warehouse', route: '/main/containers' },
+  { id: 'lots', name: 'LOTS', svgIcon: 'ontimize:containerIcon', route: '/main/lots' },
+  { id: 'transports', name: 'TRANSPORTS', icon: 'local_shipping', route: '/main/transports' },
+  { id: 'usage', name: 'USAGE', icon: 'payments', route: '/main/company-bills' },
+  { id: 'alerts', name: 'ALERTS', icon: 'priority_high', route: '/main/alerts' },
   {
     id: 'masters', name: 'MASTERS', icon: 'key',
     items: [
@@ -18,8 +18,8 @@ export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'devices-without-users', name: 'DEVICE_ASSIGNMENT', route: '/main/admin/devices-without-users', icon: 'developer_board' },
   { id: 'medidas', name: 'MEASUREMENTS', icon: 'thermostat', route: '/main/admin/medidas' },
   { id: 'companies', name: 'COMPANIES', icon: 'apartment', route: '/main/admin/companies' },
-  { id: 'consumptions', name: 'USAGE', icon: 'payments', route: '/main/admin/bills' },
   { id: 'plans', name: 'PLANS', icon: 'paid', route: '/main/admin/plans' },
+  { id: 'consumptions', name: 'USAGE', icon: 'payments', route: '/main/admin/bills' },
   { id: 'users', name: 'USERS', route: '/main/admin/users', icon: 'person' },
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
+  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' },
 ];
--- cd2024bfi1g1-model/src/main/resources/db/migration/V28__create_plan_prices_table.sql
+++ cd2024bfi1g1-model/src/main/resources/db/migration/V28__create_plan_prices_table.sql
@@ -11,11 +11,11 @@ CREATE TABLE public.plan_prices(
 );
 ALTER table public.plan_prices ADD CONSTRAINT prices_plans_fk FOREIGN KEY (pln_id) REFERENCES public.plans(pln_id);
 
-INSERT INTO plans (pln_id, pln_name)
-VALUES(1,'BASIC');
+INSERT INTO plans (pln_name)
+VALUES('BASIC');
 
-INSERT INTO public.plan_prices (pp_id,pp_fixed_price, pp_dev_price, pp_bundle_price, pp_bundle_requests, pp_start, pln_id)
-VALUES (1,4.99,5.99,20.67,1000,now(), 1);
+INSERT INTO public.plan_prices (pp_fixed_price, pp_dev_price, pp_bundle_price, pp_bundle_requests, pp_start, pln_id)
+VALUES (4.99,5.99,20.67,1000,now(), 1);
 
 ALTER TABLE public.companies ADD pln_id INTEGER;
 ALTER table public.companies ADD CONSTRAINT companies_plans_fk FOREIGN KEY (pln_id) REFERENCES public.plans(pln_id);
@@ -30,3 +30,7 @@ ALTER TABLE public.bills ALTER COLUMN pp_id SET DEFAULT 1;
 UPDATE bills
 SET pp_id = 1
 WHERE pp_id IS NULL;
+
+UPDATE public.usr_role
+SET rol_json_client_permission='{"menu": [{ "attr": "users", "visible": false, "enabled": false },{ "attr": "companies", "visible": false, "enabled": false },{ "attr": "devices-without-users", "visible": false, "enabled": false },{ "attr": "medidas", "visible": false, "enabled": false },{ "attr": "consumptions", "visible": false, "enabled": false },{ "attr": "plans", "visible": false, "enabled": false }]}'
+WHERE rol_id=2;
