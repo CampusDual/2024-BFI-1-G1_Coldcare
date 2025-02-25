d44ace43d607b122b1ddd8598bb6ce01b3a95163
Jorge González Fernández <j.gonzalezf@udc.es>
2025-02-25 15:07:54 +0100

    Revert "Default text-inputs on containers details"
    This reverts commit 9e662734851d40071d9a7788f9dd94c266935299.

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-details/containers-details.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/containers/containers-details/containers-details.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { OTextInputComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-containers-details',
@@ -8,27 +7,7 @@ import { OTextInputComponent } from 'ontimize-web-ngx';
   styleUrls: ['./containers-details.component.css']
 })
 
-export class ContainersDetailsComponent implements OnInit {
-
-  @ViewChild('AVGT', { static: false }) AVGT: OTextInputComponent;
-  @ViewChild('MAXT', { static: false }) MAXT: OTextInputComponent;
-  @ViewChild('MINT', { static: false }) MINT: OTextInputComponent;
-  @ViewChild('AVGH', { static: false }) AVGH: OTextInputComponent;
-  @ViewChild('MAXH', { static: false }) MAXH: OTextInputComponent;
-  @ViewChild('MINH', { static: false }) MINH: OTextInputComponent;
-  @ViewChild('ALERTS', { static: false }) ALERTS: OTextInputComponent;
-
-  ngOnInit(): void {
-    setInterval(() => {
-      this.AVGT.setValue("N/A", { emitModelToViewValueChange: false });
-      this.MAXT.setValue("N/A", { emitModelToViewValueChange: false });
-      this.MINT.setValue("N/A", { emitModelToViewValueChange: false });
-      this.AVGH.setValue("N/A", { emitModelToViewValueChange: false });
-      this.MAXH.setValue("N/A", { emitModelToViewValueChange: false });
-      this.MINH.setValue("N/A", { emitModelToViewValueChange: false });
-      this.ALERTS.setValue("0", { emitModelToViewValueChange: false });
-    }, 1000);
-  }
+export class ContainersDetailsComponent {
 
   constructor(
     private router: Router
