e6c6a358e1751fdea5ecfd3bdad164e3d07d74aa
Alex Domínguez <doalex7@gmail.com>
2025-02-28 14:28:41 +0100

    Added filters

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/bills/bills-home/bills-home.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/admin/bills/bills-home/bills-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Expression, FilterExpressionUtils } from "ontimize-web-ngx";
 
 @Component({
   selector: 'app-bills-home',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./bills-home.component.css']
 })
 export class BillsHomeComponent {
+  createFilter(values: Array<{ attr: string; value: any }>): Expression {
+    let filters: Array<Expression> = [];
+
+    values.forEach((fil) => {
+      if (fil.value) {
+        switch (fil.attr) {
+          case "BIL_YEAR":
+            // Filtrar por ID del dispositivo
+            filters.push(
+              FilterExpressionUtils.buildExpressionEquals("BIL_YEAR", fil.value)
+            );
+            break;
+
+          case "BIL_MONTH":
+            // Filtrar por ID del dispositivo
+            filters.push(
+              FilterExpressionUtils.buildExpressionEquals("BIL_MONTH", fil.value)
+            );
+            break;
+
+          case "CMP_NAME":
+            // Filtrar por ID del dispositivo
+            filters.push(
+              FilterExpressionUtils.buildExpressionEquals("CMP_NAME", fil.value)
+            );
+            break;
+
+          default:
+            console.warn(`Atributo desconocido: ${fil.attr}`);
+            break;
+        }
+      }
+    });
+
+    // Combinar los filtros con el operador AND si hay más de uno
+    if (filters.length > 0) {
+      return filters.reduce((exp1, exp2) =>
+        FilterExpressionUtils.buildComplexExpression(
+          exp1,
+          exp2,
+          FilterExpressionUtils.OP_AND
+        )
+      );
+    } else {
+      return null; // Sin filtros aplicados
+    }
+  }
 
 }
