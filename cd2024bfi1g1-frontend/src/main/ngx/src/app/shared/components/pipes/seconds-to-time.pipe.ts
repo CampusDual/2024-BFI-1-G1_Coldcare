721432c63dace95848126d63cffb5c33de344dfe
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2025-03-03 13:48:46 +0100

    Correccion para campos vacios

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/components/pipes/seconds-to-time.pipe.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/components/pipes/seconds-to-time.pipe.ts
@@ -7,11 +7,13 @@ export class SecondsToTimePipe implements PipeTransform {
 
   transform(value: number): string {
 
+    if (!value || value < 0) return '';
+
     const days = Math.floor(value / 86400);
     const hours = Math.floor((value % 86400) / 3600);
     const minutes = Math.floor((value % 3600) / 60);
     const secs = Math.floor(value % 60);
 
-    return `${days} d ${hours.toString().padStart(2, '0')} h ${minutes.toString().padStart(2, '0')} min ${secs.toString().padStart(2, '0')} seg`;
+    return `${days} d, ${hours.toString().padStart(2, '0')} h ${minutes.toString().padStart(2, '0')} min ${secs.toString().padStart(2, '0')} seg`;
   }
 }
