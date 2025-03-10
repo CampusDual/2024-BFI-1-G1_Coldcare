3fa357d65a969ff4a2b3dfa2b503144a3f669ea4
Gabriel <Gabrieldb050890@gmail.com>
2025-03-10 15:02:23 +0100

    obtain pending alerts for title

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/app.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, Injector } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppearanceService, OntimizeMatIconRegistry } from 'ontimize-web-ngx';
 import { GeolocationService } from './services/geolocation.service';
+import { AlertaService } from './services/alerta.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'o-app',
@@ -9,19 +11,6 @@ import { GeolocationService } from './services/geolocation.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  ontimizeMatIconRegistry: OntimizeMatIconRegistry;
-
-  ngOnInit() {
-    if (this.ontimizeMatIconRegistry.addOntimizeSvgIcon) {
-      this.ontimizeMatIconRegistry.addOntimizeSvgIcon('containerIcon', 'assets/icons/container.svg');
-    }
-
-    if (this.geolocationService.checkTrackingStatus()) {
-      console.log('Tracking activo al recargar. Reiniciando...');
-      const trpId = Number(localStorage.getItem('TRP_ID'));
-      this.geolocationService.continueTracking(trpId);
-    }
-  }
 
   constructor(private router: Router, protected appearanceService: AppearanceService, protected injector: Injector, private geolocationService: GeolocationService,
   ) {
@@ -33,4 +22,41 @@ export class AppComponent {
     }
   }
 
+  ontimizeMatIconRegistry: OntimizeMatIconRegistry;
+  alertaService: AlertaService
+  titleService: Title
+
+  ngOnInit() {
+    if (this.ontimizeMatIconRegistry.addOntimizeSvgIcon) {
+      this.ontimizeMatIconRegistry.addOntimizeSvgIcon('containerIcon', 'assets/icons/container.svg');
+    }
+
+    if (this.geolocationService.checkTrackingStatus()) {
+      console.log('Tracking activo al recargar. Reiniciando...');
+      const trpId = Number(localStorage.getItem('TRP_ID'));
+      this.geolocationService.continueTracking(trpId);
+    }
+
+
+
+    setInterval(() => {
+      this.obtenerAlertas();
+    }, 5000);
+  }
+
+  altIds = [1, 2, 3];
+  obtenerAlertas(): void {
+    this.alertaService.obtenerAlertasPendientes().subscribe((alertas) => {
+      const cantidadAlertas = alertas.filter(alerta => alerta.ESTADO === 'pendiente').length;
+
+      // Cambia el título de la pestaña para reflejar el número de alertas
+      if (cantidadAlertas > 0) {
+        this.titleService.setTitle(`Tienes ${cantidadAlertas} alertas pendientes`);
+      } else {
+        this.titleService.setTitle('No tienes alertas pendientes');
+      }
+    });
+  }
 }
+
+
