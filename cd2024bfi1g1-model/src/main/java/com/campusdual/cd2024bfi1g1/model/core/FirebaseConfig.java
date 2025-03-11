198c45969a33a764fd0c8cf1def174b57797cb34
IagoOlivares <iago2002oc@gmail.com>
2025-03-11 14:05:01 +0100

    yml firebase configuration

--- cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/FirebaseConfig.java
+++ cd2024bfi1g1-model/src/main/java/com/campusdual/cd2024bfi1g1/model/core/FirebaseConfig.java
@@ -4,19 +4,22 @@ package com.campusdual.cd2024bfi1g1.model.core;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 @Configuration
 public class FirebaseConfig {
+    @Value("${firebase.serviceAccountKey}")
+    private String FIREBASE_SERVICE_ACCOUNT_KEY;
 
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("cd2024bfi1g1-frontend\\src\\main\\ngx\\src\\assets\\coldcarebfi1g1-firebase.json");
+        FileInputStream serviceAccount = new FileInputStream("cd2024bfi1g1-model/src/main/resources/coldcarebfi1g1-firebase-adminsdk-fbsvc-f7b1a0ea2a.json");
 
         if (serviceAccount == null) {
             throw new IOException("No se encontró el archivo firebase-credentials.json en resources/");
