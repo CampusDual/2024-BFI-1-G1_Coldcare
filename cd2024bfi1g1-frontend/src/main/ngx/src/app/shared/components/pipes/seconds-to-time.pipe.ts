92d5fb185bf3896c693f2ec4ba4693da3411a0a4
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2025-03-13 14:41:16 +0100

    Modificacion del campo estado

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/components/pipes/seconds-to-time.pipe.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/shared/components/pipes/seconds-to-time.pipe.ts
@@ -10,11 +10,10 @@ export class SecondsToTimePipe implements PipeTransform {
 
     if (!value || value < 0) return '';
 
-    const days = Math.floor(value / 86400);
-    const hours = Math.floor((value % 86400) / 3600);
+    const hours = Math.floor(value / 3600);
     const minutes = Math.floor((value % 3600) / 60);
     const secs = Math.floor(value % 60);
 
-    return `${days} d ${hours.toString().padStart(2, '0')} h ${minutes.toString().padStart(2, '0')} min ${secs.toString().padStart(2, '0')} s`;
+    return `${hours.toString().padStart(2, '0')} h ${minutes.toString().padStart(2, '0')} min ${secs.toString().padStart(2, '0')} s`;
   }
 }
