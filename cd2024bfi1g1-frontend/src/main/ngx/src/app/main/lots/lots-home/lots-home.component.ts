0047cc70de450400868f0da96f96fdb8bb7ecf8a
Hugo <hugonvsg@gmail.com>
2025-03-13 13:39:42 +0100

    fixed table on lots and transports

--- cd2024bfi1g1-frontend/src/main/ngx/src/app/main/lots/lots-home/lots-home.component.ts
+++ cd2024bfi1g1-frontend/src/main/ngx/src/app/main/lots/lots-home/lots-home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { OTableComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-lots-home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./lots-home.component.css']
 })
 export class LotsHomeComponent {
-  
+
+  @ViewChild('lotsTable', { static: false }) lotsTable!: OTableComponent;
+
+  fillData() {
+    let array = this.lotsTable.getDataArray();
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].HAS_REPITED === undefined)
+        array[i].HAS_REPITED = "0";
+    }
+  }
 }
